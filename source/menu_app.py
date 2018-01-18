f9adfdc5474acc3c94a7237240bb2bbc514845fd
DRpandaMD <michael.zarate.ce@gmail.com>
2018-01-18 13:11:00 -0600

    finished creating routes for 8 pages

--- source/menu_app.py
+++ source/menu_app.py
@@ -21,9 +21,66 @@ session = DBSession()
 # display all restaurants and root
 @app.route('/')
 @app.route('/restaurants/')
-def resaurantDefault():
+def restaurant_default():
     output = ""
     output += "Here is the Default Landing Page"
+    return output
+
+
+# Create a new restaurant
+@app.route('/restaurants/new/')
+def restaurant_new():
+    output = ""
+    output += "Here is the New Restaurant Page"
+    return output
+
+
+# Edit a existing restaurant
+@app.route('/restaurants/<int:restaurant_id>/edit/')
+def restaurant_edit(restaurant_id):
+    output = ""
+    output += "Here is the Edit Restaurant Page"
+    return output
+
+
+# Delete a specified restaurant
+@app.route('/restaurants/<int:restaurant_id>/delete/')
+def restaurant_delete(restaurant_id):
+    output = ""
+    output += "Here is the Delete Restaurant Page"
+    return output
+
+
+# show a specific restaurant's menu
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurant_show_menu(restaurant_id):
+    output = ""
+    output += "Here is the Menu for 'x' Restaurant Page"
+    return output
+
+
+# add a new menu item to restaurant
+@app.route('/restaurants/<int:restaurant_id>/menu/new/')
+def restaurant_new_menu_item(restaurant_id):
+    output = ""
+    output += "Here is the new menu item for 'x' Restaurant Page"
+    return output
+
+
+# add a edit menu item to restaurant
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit/')
+def restaurant_edit_menu_item(restaurant_id, menu_id):
+    output = ""
+    output += "Here is the edit menu item for 'x' Restaurant Page"
+    return output
+
+
+# add a edit menu item to restaurant
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete/')
+def restaurant_delete_menu_item(restaurant_id, menu_id):
+    output = ""
+    output += "Here is the delete menu item for 'x' Restaurant Page"
     return output
 
 
